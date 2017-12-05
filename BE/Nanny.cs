0f96ee168b9e70e1a19426f7eaf76318c495a22b
user <user@HP>
2017-12-05 23:14:23 +0200

    ...

--- BE/Nanny.cs
+++ BE/Nanny.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace BE
 {
+
     public class Nanny
     {
         //------------proportis and fields----------
@@ -14,5 +15,17 @@ namespace BE
         public string last_name { get; set; }
         public DateTime date_of_birth { get; set; }
         public string phone_number { get; set; }
+        public string address { get; set; }
+        public bool elevator_exists { get; set; }
+        public int floor { get; set; }
+        public int years_experience { get; set; }
+        public int Max_num_child { get; set; }
+        public int Min_age_child { get; set; }
+        public int Max_age_child { get; set; }
+        public bool allow_rate_per_hour { get; set; }
+        public double hourly_rate { get; set; }
+        public double monthly_rate { get; set; }
+        private Work_Schedule[] work_schedule = new Work_Schedule[(int)days.size];
+
     }
 }
---
+++ BE/enums and stracts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    enum days { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, size }
+
+    struct Work_Schedule
+    {
+        bool works;
+        DateTime start_hour;
+        DateTime end_hour;
+    }
+}
