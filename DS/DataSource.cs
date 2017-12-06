6fbd4217d0792bba7332ecd0f9d36315b1e30993
user <user@HP>
2017-12-06 19:54:14 +0200

    נריה: עידכון מקור נתונים

--- DS/DataSource.cs
+++ DS/DataSource.cs
@@ -6,8 +6,11 @@ using System.Threading.Tasks;
 
 namespace DS
 {
-    public class DataSource
+    public static class DataSource
     {
-        public static List<BE>
+        public static List<BE.Nanny> Nanny_list=new List<BE.Nanny>();
+        public static List<BE.Mother> Mother_list=new List<BE.Mother>();
+        public static List<BE.Child> Child_list=new List<BE.Child>();
+        public static List<BE.Contract> Contract_list = new List<BE.Contract>();
     }
 }
